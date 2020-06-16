3c2504f87570bf09909bd86c4032703afe656748
Khushal Thepane <khushalthepane20002gmail.com>
2020-06-16 23:53:01 +0530

    Project Done

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,14 +1,18 @@
 from django.db import models
 from django.utils import timezone
-from django.core.urlresolvers import reverse
+from django.urls import reverse
+# SuperUserInformation
+# User: Jose
+# Email: training@pieriandata.com
+# Password: testpassword
 
 # Create your models here.
 class Post(models.Model):
     author = models.ForeignKey('auth.User',on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
-    create_date = models.DateTimeField(default=timezone.now())
-    published_date = models.DateTimeField(blank=True,null=True)
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
 
     def publish(self):
         self.published_date = timezone.now()
@@ -23,11 +27,12 @@ class Post(models.Model):
     def __str__(self):
         return self.title
 
+
 class Comment(models.Model):
-    post = models.ForeignKey('blog.Post',related_name='comments',on_delete=models.CASCADE)
+    post = models.ForeignKey('blog.Post', related_name='comments',on_delete=models.CASCADE)
     author = models.CharField(max_length=200)
     text = models.TextField()
-    create_date = models.DateTimeField(default=timezone.now())
+    created_date = models.DateTimeField(default=timezone.now)
     approved_comment = models.BooleanField(default=False)
 
     def approve(self):
@@ -38,4 +43,4 @@ class Comment(models.Model):
         return reverse("post_list")
 
     def __str__(self):
-        return self.title
+        return self.text
